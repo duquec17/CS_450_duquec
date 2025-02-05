328ab614b299ab5f29bb7e0de036f80d3ce21cf0
Cristian <duquec@sunypoly.edu>
2025-02-05 16:09:19 -0500

    commit

--- src/app/Exercise03.cpp
+++ src/app/Exercise03.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <vulkan/vulkan.hpp>
 #include "VkBootstrap.h"
+#define GLFW_INCLUDE_NONE
+#include <
+
 using namespace std;
 
 int main(int argc, char **arg) {
@@ -20,9 +23,50 @@ int main(int argc, char **arg) {
     vkb::Instance vkbInstance = instRes.value();
     vk::Instance instance = vk::Instance {vkbInstance.instance};
 
-    // TODO
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "BEHOLD",
+                                            nullptr, nullptr);
+
+    vkSurfaceKHR vkSurface = nullptr;
+    auto surfRes = glfwCreateWindowSurface(vkbInstance.instance,
+                                            window, nullptr,
+                                            &vkSurface);
+    vk::SurfaceKHR surface = vk::SurfaceKHR {vkSurface};
+    
+    vkb::PhysicalDeviceSelector phySelect {vkbInstance};
+    auto pppphyRet = phySelect.set_surface(vkSurface)
+                                .set_minimum_version(1,1)
+                                .select();
+    vkb::PhysicalDevice vkbPhyDevice =phyRet.value();
+    vk::PhysicalDevice PhyDevice
+        = vk::PhysicalDevice(vkbPhyDevice.physical_device);
+
+    vkb::DeviceBuilder devBuilder {vkbPhyDevice};
+    auto devRet = devBuilder.build();
+    vkb::Device vkbDevice = devRet.value();
+    vk::Device device = vk::Device {vkbDevice.device};
+
+    auto graphQRet = vkbDevice.get_queue(vkb::QueueType::graphics);
+    auto presQRet = vkbDevice.get_queue(vkb::QueueType::present);
+
+    
     
 
+    // CREATION TODO
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+
+        // RENDER TODO
+    }
+
+    // TODO
+
+    // CLEANUP TODO
+    instance.destroySurfaceKHR(surface);
+    glfwDestroyWindow(window);
+    glfwTerminate();
     vkb::destroy_instance(vkbInstance);
 
     return 0;
