7a09714ac2ceab9d5123eafa6ff9a4eaa9f4a8b8
Cristian <duquec@sunypoly.edu>
2025-02-05 16:12:32 -0500

    exercise fix

--- src/app/Exercise03.cpp
+++ src/app/Exercise03.cpp
@@ -3,14 +3,13 @@
 #include <vulkan/vulkan.hpp>
 #include "VkBootstrap.h"
 #define GLFW_INCLUDE_NONE
-#include <
-
+#include <GLFW/glfw3.h>
 using namespace std;
 
-int main(int argc, char **arg) {
+int main(int argc, char **argv) {
 
     if(argc >= 2) {
-        string filename = string(arg[1]);
+        string filename = string(argv[1]);
         cout << "FILENAME: " << filename << endl;
     }
 
@@ -21,49 +20,59 @@ int main(int argc, char **arg) {
                             .use_default_debug_messenger()
                             .build();
     vkb::Instance vkbInstance = instRes.value();
-    vk::Instance instance = vk::Instance {vkbInstance.instance};
+    vk::Instance instance = vk::Instance { vkbInstance.instance };
 
     glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "BEHOLD",
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "BEHOLD", 
                                             nullptr, nullptr);
 
-    vkSurfaceKHR vkSurface = nullptr;
+    VkSurfaceKHR vkSurface = nullptr;
     auto surfRes = glfwCreateWindowSurface(vkbInstance.instance,
                                             window, nullptr,
                                             &vkSurface);
-    vk::SurfaceKHR surface = vk::SurfaceKHR {vkSurface};
-    
-    vkb::PhysicalDeviceSelector phySelect {vkbInstance};
-    auto pppphyRet = phySelect.set_surface(vkSurface)
-                                .set_minimum_version(1,1)
-                                .select();
-    vkb::PhysicalDevice vkbPhyDevice =phyRet.value();
-    vk::PhysicalDevice PhyDevice
-        = vk::PhysicalDevice(vkbPhyDevice.physical_device);
+    vk::SurfaceKHR surface = vk::SurfaceKHR { vkSurface };
 
-    vkb::DeviceBuilder devBuilder {vkbPhyDevice};
+    vkb::PhysicalDeviceSelector phySelect { vkbInstance };
+    auto phyRet = phySelect.set_surface(vkSurface)
+                            .set_minimum_version(1,1)
+                            .select();
+    vkb::PhysicalDevice vkbPhyDevice = phyRet.value();
+    vk::PhysicalDevice phyDevice
+         = vk::PhysicalDevice { vkbPhyDevice.physical_device };
+
+    cout << "DEVICE: " << phyDevice.getProperties().deviceName << endl;
+
+    vkb::DeviceBuilder devBuilder { vkbPhyDevice };
     auto devRet = devBuilder.build();
     vkb::Device vkbDevice = devRet.value();
-    vk::Device device = vk::Device {vkbDevice.device};
+    vk::Device device = vk::Device { vkbDevice.device };
 
     auto graphQRet = vkbDevice.get_queue(vkb::QueueType::graphics);
     auto presQRet = vkbDevice.get_queue(vkb::QueueType::present);
 
-    
-    
+    vk::Queue graphQueue = vk::Queue { graphQRet.value() };
+    vk::Queue presentQueue = vk::Queue { presQRet.value() };
+
+    auto graphIndex 
+        = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+    auto presentIndex
+        = vkbDevice.get_queue_index(vkb::QueueType::present).value();
+
+    cout << "Graphics queue: " << graphIndex << endl;
+    cout << "Present queue: " << presentIndex << endl;
+
 
     // CREATION TODO
 
-    while(!glfwWindowShouldClose(window)){
+    while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         // RENDER TODO
     }
 
-    // TODO
-
     // CLEANUP TODO
+    device.destroy();
     instance.destroySurfaceKHR(surface);
     glfwDestroyWindow(window);
     glfwTerminate();
